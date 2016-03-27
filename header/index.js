d86357955c21a6b0e8b2072673344bcd3efcc840
mAAdhaTTah <jamesorodig@gmail.com>
2016-03-26 22:50:11 -0400

    Use props to display connection status

--- header/index.js
+++ header/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { WAITING, CONNECTED, DISCONNECTED } from '../messages/connection';
 
 const Header = React.createClass({
     displayName: 'Header',
 
     render: function render() {
         let display = '';
+
+        const { status } = this.props;
+
+        switch (status) {
+            case CONNECTED:
+                display = 'Connected';
+                break;
+            case DISCONNECTED:
+                display = 'Disconnected';
+                break;
+            case WAITING:
+            default:
+                display = 'Waiting'
+                break;
+        }
 
         return (
             <header className="ctr-header-container">
